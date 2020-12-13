2bac2d95ec2a7b947131767dd85349dca4233a68
Heldão <orfeowrk@gmail.com>
2020-12-13 15:15:29 -0300

    introduzindo javascript

---
+++ Atividade/code.js
@@ -0,0 +1,7 @@
+let rodape = document.getElementById('rodape')
+rodape.setAttribute("style", "text-align: center")
+rodape.innerHTML = "Feito por Hélder Silva Ferreira Lima" 
+
+let acess = document.getElementById('acesso')
+acess.setAttribute("style", "text-align: center")
+acess.innerHTML = "Acessado em: " + Date()
